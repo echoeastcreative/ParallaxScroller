2b6019a5dda0a34690b8f936439cdd0170783389
Doug Hurst <dalan.hurst@gmail.com>
2011-03-15 18:53:47 -0500

    Cleaned up some variable names. Hopefully this is easier to follow now. Still sluggish in Chrome, will experiment with triggers next.

--- src/ParallaxScroller.js
+++ src/ParallaxScroller.js
@@ -38,69 +38,63 @@ ParallaxScroller.prototype.getBackgroundImageHeight = function(url) {
  * Sets a layer to be scrolled in parallax with other layers
  * 
  * @param string layer_selector CSS Selector for designated segments of the layer
- * @param float speed Multiplier for parallaxing speed & direction
+ * @param float speed Multiplier for speed & direction
  * @param string type One of 'background' or 'element'
- * @param int algorithm Algorithm number to use
  * @return this Provides fluent interface
  */
-ParallaxScroller.prototype.addLayer = function(layer_selector, speed, type, algorithm) {
+ParallaxScroller.prototype.addLayer = function(layer_selector, speed, type) {
 	var segments = [];
 	if(typeof(type) === 'undefined') type = 'element';
-	if(typeof(algorithm) === 'undefined') algorithm = 1;
 
 	//  Add segments to array
-	$(layer_selector).each($.proxy(function(index) {
+	$(layer_selector).each(function(index) {
+		var self = $(this);
 
 		//  Handle background segments
-		//  These will be parallaxed by offsetting background-position
-		//  NOTE: The x-value of the background position *must* be in pixels
-		if(type == 'background' && $($(layer_selector)[index]).css('background-image') != 'none') {
+		//  These will be in parallax by background-position offset
+		//  NOTE: If set, the x-value of the background position *must* be in pixels
+		if(type == 'background' && self.css('background-image') != 'none') {
+			var container = self;
+
 			//  Calculate starting left value for background image
-			var left = $($(layer_selector)[index])
+			var left = self
 				.css('background-position')
-				.substr(
-						0,
-						$($(layer_selector)[index])
-							.css('background-position')
-							.indexOf(' ')
-				);
+				.substr(0, self.css('background-position').indexOf(' '));
 			if(left.indexOf('px') == -1) left = '0';
 			else left = left.replace('px', '');
 
 			//  Calculate starting top value for background image
 			var top = 0;
-			if(speed < 0 ) top = -1 * (this.getBackgroundImageHeight($($(layer_selector)[index]).css('background-image')) - $(window).height());
-			else top = $($(layer_selector)[index]).offset().top * speed * -1;
+			if(speed < 0 ) top = -1 * (this.getBackgroundImageHeight(self.css('background-image')) - $(window).height());
+			else top = self.offset().top * speed * -1;
 
-			$($(layer_selector)[index])
+			var self_start = {left: left, top: top};
+
+			//  Set starting values & set segment height to DOMWindow height
+			self
 				.css('background-repeat', 'no-repeat')
 				.css('background-position', left + 'px ' + top + 'px')
 				.css('height', $(window).height() + 'px');
-
-			segments[segments.length] = {
-					container: $(layer_selector)[index],
-					element: $(layer_selector)[index],
-					background_start: {left: left, top: top},
-					starting_offset: $($(layer_selector)[index]).offset(),
-					type: type
-			};
+			$(window).bind('scroll', self.ParallaxBackgroundImage);
 		}
 
 		//  Handle element segments
 		if(type == 'element') {
-			segments[segments.length] = {
-					container: $(layer_selector)[index].parentNode,
-					element: $(layer_selector)[index],
-					element_start: $($(layer_selector)[index]).offset(),
-					starting_offset: $($(layer_selector)[index].parentNode).offset(),
-					type: type
-			};
+			var container = $(this.parentNode);
+			var self_start = self.offset();
 		}
-	}, this));
+
+		segments[segments.length] = {
+				container: container,
+				element: self,
+				self_start: self_start,
+				starting_offset: container.offset(),
+				type: type
+		};
+	});
 
 	//  Create layer
 	var layer = {
-		algorithm: algorithm,
 		segments: segments,
 		selector: layer_selector,
 		speed: speed
@@ -116,66 +110,35 @@ ParallaxScroller.prototype.addLayer = function(layer_selector, speed, type, algo
  * @param event The scroll event
  */
 ParallaxScroller.prototype.parallax = function(event) {
-	scroll_top = $(window).scrollTop();
+	var distance = $(window).scrollTop();
 
 	//  Loop through layers
 	for(i in this.layers) {
+		var layer = this.layers[i];
 
 		//  Loop through layer segments
-		for(j = 0; j < this.layers[i].segments.length; j++) {
-//			if(!this.isInView(this.layers[i].segments[j])) continue;
-
-			vertical_offset = Math.floor((this.layers[i].segments[j].starting_offset.top - scroll_top) * this.layers[i].speed);
+		for(j = 0; j < layer.segments.length; j++) {
+			var segment = layer.segments[j];
+			var vertical_offset = Math.floor((segment.starting_offset.top - distance) * layer.speed);
 
 			//  Set offset
-			switch(this.layers[i].segments[j].type) {
+			switch(segment.type) {
 				case 'background':
 					var top;
-					if(this.layers[i].speed < 0) top = this.layers[i].segments[j].background_start.top + vertical_offset;
+					if(layer.speed < 0) top = segment.self_start.top + vertical_offset;
 					else top = -1 * vertical_offset;
-
-//					console.log({
-//						vertical_offset: vertical_offset,
-//						top: top,
-//						scroll_top: scroll_top,
-//						background_start: this.layers[i].segments[j].background_start.top,
-//						starting_top: this.layers[i].segments[j].starting_offset.top
-//					});
-
-					$(this.layers[i].segments[j].element)
-						.css('background-position', this.layers[i].segments[j].background_start.left + 'px ' + top + 'px');
+					$(segment.element)
+						.css('background-position', segment.self_start.left + 'px ' + top + 'px');
 					break;
 				default:
-					if(this.layers[i].speed < 0) top = this.layers[i].segments[j].element_start.top - vertical_offset;
-					else top = this.layers[i].segments[j].element_start.top - vertical_offset;
-
-//					console.log({
-//						vertical_offset: vertical_offset,
-//						top: top,
-//						scroll_top: scroll_top,
-//						element_start: this.layers[i].segments[j].element_start.top,
-//						starting_top: this.layers[i].segments[j].starting_offset.top
-//					});
-
-					$(this.layers[i].segments[j].element).offset({
+					if(layer.speed < 0) top = segment.self_start.top - vertical_offset;
+					else top = segment.self_start.top - vertical_offset;
+					$(segment.element).offset({
 						top: top,
-						left: this.layers[i].segments[j].starting_offset.left
+						left: segment.starting_offset.left
 					});
 					break;
 			}
 		}
 	}
-};
-
-/**
- * Check if a segment is within the viewport
- */
-ParallaxScroller.prototype.isInView = function(segment) {
-	return (
-			segment.starting_offset.top > $(window).scrollTop() &&
-			segment.starting_offset.top < ($(window).scrollTop() + this.viewport_height)
-		) || (
-			($(segment.container).height() + segment.starting_offset.top) > $(window).scrollTop() &&
-			($(segment.container).height() + segment.starting_offset.top) < ($(window).scrollTop() + this.viewport_height)
-		);
 };
